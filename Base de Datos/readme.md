df2da2520b27d58a3c845f49c47a947486e7f778
carlosferrersanchez <cfs260501@gmail.com>
2023-11-28 09:59:08 +0100

    reestructuración carpetas BBDD + script para iniciar

---
+++ Base de Datos/readme.md
@@ -0,0 +1,6 @@
+Base de datos conectada y funcionando en localhost
+
+  Para conectar:
+  - cd Base de Datos
+  - chmod +x script.sh 
+  - ./script.sh
---
+++ Base de Datos/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Otorgar permisos de ejecución al script Python cada vez que se ejecute este script
+chmod +x mysql_server/conexion.py
+
+# Ir al directorio donde se encuentra el docker-compose.yml y el script Python
+cd mysql_server
+
+# Iniciar los contenedores definidos en el docker-compose.yml
+docker-compose up -d
+
+# Esperar un momento para asegurarse de que el contenedor esté en funcionamiento
+sleep 5
+
+# Ejecutar tu script de Python que establece la conexión automáticamente
+python conexion.py
+
+
