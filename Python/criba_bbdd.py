a3c81d78ad2c9d46cbb4cc31857c7dddd38e790c
carlosferrersanchez <cfs260501@gmail.com>
2023-12-03 16:01:50 +0100

    Script de criba on + reestructuraciones

--- Python/criba_bbdd.py
+++ Python/criba_bbdd.py
@@ -6,11 +6,33 @@ username = 'user'
 password = 'admin01'
 
 # Funciones para las características excluyentes #
+        # NO españoles que NO residan en España
+        # SI españoles que NO reciban algun tipo de pensión
+        # Pensionistas de viudedad de menos de 55 años
+        # Desempleados / Otro tipo de pensionistas de menos de 60 años
+        # Beneficiario de la SS menores de 65 años
 
-def excluye_no_espanoles_fuera_espana(cursor):
-    # Excluye a los NO españoles que NO residan en España
-    consulta = "DELETE FROM personas WHERE nacionalidad != 'España' AND pais_residencia != 'España'"
+def no_espanoles_fuera_espana(cursor):
+    consulta = "DELETE FROM personas WHERE Nacionalidad != 'España' AND Pais_residencia != 'España'"
     cursor.execute(consulta)
+
+def españoles_sin_pension(cursor):
+    consulta = "DELETE FROM personas WHERE nacionalidad = 'España' AND Pension != 'Titular SS' AND Pension != 'Desempleo/Otros subsidios' AND Pension != 'Jubilacion' AND Pension != 'Viuedad' AND Pension != 'Extranjero'"
+    cursor.execute(consulta)
+
+def pension_viudedad_menor_55(cursor):
+    consulta = "DELETE FROM personas WHERE Pension = 'Viudead' AND EDAD < 55"
+    cursor.execute(consulta)
+
+def desempleados_menor_60(cursor):
+    consulta = "DELETE FROM personas WHERE Pension = 'Desempleo/Otros subsidios' AND EDAD < 60"
+    cursor.execute(consulta)
+
+def beneficiarios_ss_menores_65(cursor):
+    consulta = "DELETE FROM personas WHERE Pension = 'Titular SS' AND EDAD < 65"
+    cursor.execute(consulta)
+
+
 
 try:
     conexion = pymysql.connect(
@@ -28,7 +50,11 @@ try:
         cursor = conexion.cursor()
 
         # Ejecutar las funciones para excluir personas según características
-        excluye_no_espanoles_fuera_espana(cursor)
+        no_espanoles_fuera_espana(cursor)
+        españoles_sin_pension(cursor)
+        pension_viudedad_menor_55(cursor)
+        desempleados_menor_60(cursor)
+        beneficiarios_ss_menores_65(cursor)
 
         # Confirmar los cambios en la base de datos
         conexion.commit()
