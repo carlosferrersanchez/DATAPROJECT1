57e7a9a46056f146832e1ad647a68f226acf844a
carlosferrersanchez <cfs260501@gmail.com>
2023-12-10 17:03:09 +0100

    Update personas.py

--- python/scripts/personas.py
+++ python/scripts/personas.py
@@ -185,9 +185,9 @@ def personas():
   fake = Faker('es_ES')
   gender_detector = Detector()
 
-  personas_imserso = [generar_identidad() for _ in range(1000)]
+  personas_imserso = [generar_identidad() for _ in range(20000)]
   numero_extranjeros = random.randint(0,50)
-  for i in range(1000 - numero_extranjeros, 1000):
+  for i in range(20000 - numero_extranjeros, 20000):
     fake = Faker()
     personas_imserso[i]["Segundo_apellido"] = None
     personas_imserso[i]["Nombre"] = fake.first_name()
