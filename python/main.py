2df3d38ade112581bf3674a5d17db38ef9391614
carlosferrersanchez <cfs260501@gmail.com>
2023-12-10 13:54:10 +0100

    gg

--- python/main.py
+++ python/main.py
@@ -5,6 +5,7 @@ from scripts.valoraciones import valoraciones
 from scripts.plazas_disponibles import plazas_disponibles 
 from scripts.personas import personas
 from scripts.asignacion_plazas import asignacion
+from scripts.generador_csv import exportar_a_csv
 
 personas()
 time.sleep(2)  
@@ -15,3 +16,8 @@ time.sleep(2)
 valoraciones()
 time.sleep(2)  
 asignacion()
+time.sleep(2)
+exportar_a_csv('personas', 'personas.csv')
+exportar_a_csv('lista_espera', 'lista_espera.csv')
+exportar_a_csv('plazas_disponibles', 'plazas_disponibles.csv')
+exportar_a_csv('viajes_asignados', 'viajes_asignados.csv')
---
+++ python/scripts/generador_csv.py
@@ -0,0 +1,33 @@
+import pymysql
+import pandas as pd
+
+def exportar_a_csv(tabla, nombre_archivo, ruta='./Jupyter'):
+    try:
+        # Utilizar los mismos detalles de conexión que en tus otros scripts
+        conexion = pymysql.connect(
+            host='mysql_server',
+            user='user',
+            password='admin01',
+            database='imserso_database'
+        )
+
+        if conexion.open:
+            # Leer datos de la tabla con pandas
+            consulta = f"SELECT * FROM {tabla}"
+            df = pd.read_sql_query(consulta, conexion)
+
+            # Construir la ruta completa del archivo CSV
+            ruta_completa = f"{ruta}{nombre_archivo}"
+
+            # Exportar a archivo CSV
+            df.to_csv(ruta_completa, index=False)
+            print(f"Datos de la tabla {tabla} exportados a {ruta_completa}")
+
+    except Exception as e:
+        print(f"Error al exportar datos de la tabla {tabla}: {e}")
+
+    finally:
+        if 'conexion' in locals() and conexion.open:
+            conexion.close()
+
+# Exportar datos de las tablas a archivos CSV en la carpeta 'Jupyter'
